a68cd25df6690323dd0bb948d70e7220e10d9938
noam sella <noamsella10@gmail.com>
2025-04-22 02:11:21 +0300

    added some actions(count,by-action,by-user

--- backend/src/main/java/com/analytics/backend/controller/UserActionController.java
+++ backend/src/main/java/com/analytics/backend/controller/UserActionController.java
@@ -3,11 +3,17 @@ package com.analytics.backend.controller;
 import com.analytics.backend.model.UserAction;
 import com.analytics.backend.repository.UserActionRepository;
 
+import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController //מסמן ששזה מחלקת controller שמחזירה json
 @RequestMapping("/track")
@@ -22,5 +28,37 @@ public class UserActionController {
         return "Action tracked successfully";
     }
 
+    @GetMapping("/actions") //יוצר כתובת אינטרנט חדשה action/ שמגיבה לGET
+    public List<UserAction> getAllAction(){
+        return repository.findAll();
+    }
+    @GetMapping("/stats/count")
+    public long getTotalActionsCount(){
+
+        return repository.count(); //מחזיר מספר מסמכים במסד נתונים
+    }
+
+    @GetMapping("/stats/by-action")
+    public Map<String,Long> getActionCountByName(){
+        List<UserAction> actions = repository.findAll();
+
+        return actions.stream()
+                .collect(Collectors.groupingBy(
+                        UserAction::getActionName,
+                        Collectors.counting()
+                ));
+
+    }
+
+    @GetMapping("/states/by-user")
+    public Map<String,Long> getActionCountByUser(){
+        List<UserAction> actions = repository.findAll();
+
+        return  actions.stream()
+                .collect(Collectors.groupingBy(
+                        UserAction::getUserId,
+                        Collectors.counting()
+                ));
+    }
 
 }
